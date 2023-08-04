fd673d76a4a09aada8f5908cc3c5f29e57d1838f
Thomas Szentirmay <tlszent@outlook.com>
2023-08-05 01:36:21 +1000

    created array student name functions

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,8 +1,32 @@
+import { useState } from 'react';
+
 function Landing() {
+
+    // const students = ['Erin', 'Thomas', 'Liam', 'Jeremy'];
+    const [studentName, setStudentName] = useState('');
+    const [students, setStudents] = useState(['Erin', 'Thomas', 'Liam', 'Jeremy']);
+
+    const handleNameInput = (event) => {
+        setStudentName(event.target.value);
+    }
+
+    const addStudent = () => {
+        setStudents([...students, studentName]);
+        setStudentName('');
+    }
+
     return (
         <main>
             <h1>Landing</h1>
             <p>Welcome to our React starter site</p>
+
+            <h3>Student List:</h3>
+            {students.map((student, i) => (
+                <p key={i}>{student}</p>
+            ))}
+
+            <input onChange={handleNameInput} value={studentName} type='text' placeholder='Type student name' />
+            <button onClick={addStudent}>Add Student</button>
         </main>
     )
 }
